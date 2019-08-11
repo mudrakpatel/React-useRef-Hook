a616e58e64dd2d7c7362c610fcdeeef9046d994f
mudrakpatel <mudrakpatel96@gmail.com>
2019-08-11 17:54:16 -0400

    useRef Hook - Part 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import FocusInput from './components/FocusInput';
+//import FocusInput from './components/FocusInput';
+import ClassTimer from './components/ClassTimer';
+import HookTimer from './components/HookTimer';
 
 function App() {
   return (
     <div className="App">
-      <FocusInput/>
+      {/*<FocusInput/>*/}
+      <ClassTimer/>
+      <HookTimer/>
     </div>
   );
 }
